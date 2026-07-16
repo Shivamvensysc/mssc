1ed3c9aea489c13d9fda95cba490ccb802d60b30
AchintyaDwivedi001 <achintyadwivedi735@gmail.com>
2026-07-16 17:37:28 +0530

    "Reg succesful page"

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import RegistrationForm from './pages/Registration';
 import { LoginPage, ForgotPasswordPage } from './pages/Login';
+import RegSuccess from './pages/RegSuccess';
 import './App.css';
 
 function App() {
@@ -10,6 +11,9 @@ function App() {
         <Route path="/" element={<RegistrationForm />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/forgot-password" element={<ForgotPasswordPage />} />
+        
+        {/* Added the route for your success page below */}
+        <Route path="/success" element={<RegSuccess />} />
       </Routes>
     </BrowserRouter>
   );
