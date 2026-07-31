d0aa9ab4c6074f90de1238453d6c0c25681bf14f
Shivam <shivam@vensysco.in>
2026-07-31 13:46:55 +0530

    in gender remove transgender and add other

--- src/schemas/registrationSchema.ts
+++ src/schemas/registrationSchema.ts
@@ -569,7 +569,7 @@ export const registrationSchema = z
       message: "You must confirm residency eligibility",
     }),
 
-    gender: z.enum(["male", "female","transgender"], {
+    gender: z.enum(["male", "female","other"], {
       message: "Please select a gender",
     }),
 
